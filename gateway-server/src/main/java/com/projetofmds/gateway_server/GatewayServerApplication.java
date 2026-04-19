2908a7ce7943b981fee75159785a66087ded85b5
Fabiana-mds <byahbh@gmail.com>
2026-04-19 12:50:47 -0300

    feat(infrastructure): setup api gateway and discovery server with spring boot

--- gateway-server/src/main/java/com/projetofmds/gateway_server/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/projetofmds/gateway_server/GatewayServerApplication.java
@@ -4,12 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(excludeName = {
+    "org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration",
+    "org.springframework.cloud.autoconfigure.LifecycleMvcEndpointAutoConfiguration",
+    "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration",
+    "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration",
+    "org.springframework.cloud.autoconfigure.RefreshAutoConfiguration",
+    "org.springframework.cloud.client.discovery.simple.SimpleDiscoveryClientAutoConfiguration",
+    "org.springframework.cloud.client.discovery.simple.reactive.SimpleReactiveDiscoveryClientAutoConfiguration",
+    "org.springframework.cloud.netflix.eureka.reactive.EurekaReactiveDiscoveryClientConfiguration"
+})
 @EnableDiscoveryClient
 public class GatewayServerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayServerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
 }
